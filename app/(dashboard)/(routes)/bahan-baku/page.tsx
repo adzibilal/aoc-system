129813dde96154e0c6f4c69de769d5321bf54341
adzibilal <adzibilal02@gmail.com>
2023-10-28 09:30:54 +0700

    handle report bahan baku close & open

--- app/(dashboard)/(routes)/bahan-baku/page.tsx
+++ app/(dashboard)/(routes)/bahan-baku/page.tsx
@@ -8,6 +8,8 @@ import { use, useEffect, useState } from 'react'
 import { BahanBaku } from '@prisma/client'
 import { set } from 'zod'
 import { useRouter } from 'next/navigation'
+import { Button } from '@/components/ui/button'
+import toast from 'react-hot-toast'
 
 const BahanBakuPage = () => {
     const router = useRouter()
@@ -23,6 +25,63 @@ const BahanBakuPage = () => {
         }
     }
 
+    const generateReport = (bahanBaku: BahanBaku[], tipe: string): string => {
+        const categories = {
+            "Liquid": [],
+            "Syrup": [],
+            "Powder": [],
+            "Coffee Beans": [],
+            "Tea bag": [],
+            "Other": [],
+            "Display": []
+        };
+    
+        // Categorize the items
+        bahanBaku.forEach((item) => {
+            // @ts-ignore
+            if (item.kategori && categories[item.kategori]) {
+                // @ts-ignore
+                categories[item.kategori].push(item);
+            }
+        });
+
+        let report = tipe === 'open' ? "Bismillah\nOpen\n\nStock Opname\n\n" : "Alhamdulillah\nClose\n\nStock Opname\n\n";
+
+        for (const category in categories) {
+            // @ts-ignore
+            if (categories[category].length > 0) {
+                report += `*${category}* :\n\n`;
+                
+                // @ts-ignore
+                categories[category].forEach((item) => {
+                    report += `- ${item.nama} : ${item.stok} ${item.satuan}\n`;
+                });
+    
+                report += "\n";
+            }
+        }
+    
+        return report;
+    };
+
+    const handleReport = (tipe: string) => {
+        const openReport = generateReport(bahanBaku, tipe);
+    
+        // Copy the openReport text to the clipboard
+        navigator.clipboard.writeText(openReport)
+          .then(() => {
+            toast.success('Laporan berhasil di copy')
+            console.log('Text copied to clipboard:', openReport);
+            // You can show a success message or perform any other action here
+          })
+          .catch((error) => {
+            toast.error('Laporan gagal di copy')
+            console.error('Failed to copy text to clipboard:', error);
+            // Handle the error here
+          });
+      };
+    
+
     useEffect(() => {
         setCabangId(detailCabang?.id || '')
     }, [detailCabang])
@@ -34,7 +93,13 @@ const BahanBakuPage = () => {
 
     return (
         <div className='p-6'>
-            <h1 className='text-2xl font-bold'>Data Bahan Baku</h1>
+            <div className='flex max-md:flex-col justify-between items-center'>
+                <h1 className='text-2xl font-bold'>Data Bahan Baku</h1>
+                <div className='flex gap-3 justify-between'>
+                    <Button size='sm' onClick={()=> handleReport('open')}>Laporan Open</Button>
+                    <Button size='sm' onClick={()=> handleReport('close')}>Laporan Closing</Button>
+                </div>
+            </div>
             {bahanBaku && (
                 <DataTable
                     onDataChange={getBahanBaku}
